31a95e9c3c0b6a76e5a1bae5499ee05cc50ae76f
Sebastián Márquez <sebamarquezblanco@gmail.com>
2022-10-29 18:26:28 -0300

    menu_desplegable

--- codigo.js
+++ codigo.js
@@ -3,25 +3,46 @@ let persiana = document.getElementById("persiana")
 let dispensador = document.getElementById("dispensador")
 let tacho = document.getElementById("tacho")
 let cerradura = document.getElementById("cerradura")
-function mostrar(){
-    algo = ``
-    algo += `
-    <p>hola</p>
-    `
-    project.innerHTML = algo
-}
-document.addEventListener("DOMContentLoaded",function(){
+let algo = ``
+
     persiana.addEventListener("click",function(){
-        alert("persiana")
+        algo = `<p>Precio</p>
+                <ul>
+                <li>0</li>
+                <li>1</li>
+                <li>2</li>
+                </ul>`
+            
+                project.innerHTML = algo
     })
     dispensador.addEventListener("click",function(){
-        alert("Dispensador")
+        algo = `<p>Precio</p>
+        <ul>
+        <li>franci gei</li>
+        <li>granco feo</li>
+        <li>franco gei</li>
+        </ul>`
+    
+        project.innerHTML = algo
     })
     cerradura.addEventListener("click",function(){
-        alert("cerradura")
+        algo = `<p>Precio</p>
+        <ul>
+        <li>0</li>
+        <li>1</li>
+        <li>2</li>
+        </ul>`
+    
+        project.innerHTML = algo
     })
     tacho.addEventListener("click",function(){
-        alert("tacho")
+        algo = `<p>Precio</p>
+        <ul>
+        <li>0</li>
+        <li>1</li>
+        <li>2</li>
+        </ul>`
+    
+        project.innerHTML = algo
     })
 
-})
